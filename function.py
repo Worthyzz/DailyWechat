7074073c17b25af9fa1bbe9d98df886201cc1ed3
Worthyzz <48038868+Worthyzz@users.noreply.github.com>
2023-06-27 17:50:54 +0800

    Update function.py

--- function.py
+++ function.py
@@ -30,7 +30,7 @@ def get_words():
     result = tianapi.read()
     data = result.decode('utf-8')
     dict_data = json.loads(data)
-    return dict_data.json()['result']['content']
+    return dict_data['result']['content']
 
 def get_random_color():
     return "#%06x" % random.randint(0, 0xFFFFFF)
