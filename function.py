3b88b14a7470d9bae3375e269f30fe349c48ab5c
Worthyzz <48038868+Worthyzz@users.noreply.github.com>
2023-06-27 17:04:54 +0800

    Update function.py

--- function.py
+++ function.py
@@ -17,6 +17,7 @@ def get_words():
     words = requests.get("https://api.shadiao.pro/chp")
     if words.status_code != 200:
         return get_words()
+    print(words.json()['data']['text'])
     return words.json()['data']['text']
 
 
